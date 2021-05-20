06832ffe22925182dd94c7bde958e1e164fafa1b
the-Jinxist <nerosilva522@gmail.com>
2021-05-20 15:56:57 +0100

    Added tests for adding shopping item functionality

--- app/src/androidTest/java/com/neo/testtutorial/presentation/fragments/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/neo/testtutorial/presentation/fragments/AddShoppingItemFragmentTest.kt
@@ -1,14 +1,22 @@
 package com.neo.testtutorial.presentation.fragments
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.neo.testtutorial.R
+import com.neo.testtutorial.domain.local.ShoppingItem
+import com.neo.testtutorial.getOrAwaitValue
 import com.neo.testtutorial.launchFragmentInHiltContainer
+import com.neo.testtutorial.presentation.utils.ShoppingFragmentFactory
+import com.neo.testtutorial.presentation.viewmodels.ShoppingViewModel
+import com.neo.testtutorial.repositories.FakeShoppingRepositoryAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
@@ -18,6 +26,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @ExperimentalCoroutinesApi
@@ -27,12 +36,40 @@ class AddShoppingItemFragmentTest {
     @get:Rule
     var hiltAndroidRule = HiltAndroidRule(this)
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     lateinit var navController: NavController
 
     @Before
     fun setup(){
         hiltAndroidRule.inject()
         navController = mock(NavController::class.java)
+    }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb(){
+        val testViewModel =  ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+
+            shoppingViewModel = testViewModel
+
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5.5"))
+
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("shopping item", 5.5f, 5, ""))
     }
 
     @Test
--- app/src/main/java/com/neo/testtutorial/presentation/fragments/AddShoppingItemFragment.kt
+++ app/src/main/java/com/neo/testtutorial/presentation/fragments/AddShoppingItemFragment.kt
@@ -5,13 +5,21 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.neo.testtutorial.R
+import com.neo.testtutorial.data.network.Status
 import com.neo.testtutorial.presentation.viewmodels.ShoppingViewModel
 import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
+import kotlinx.android.synthetic.main.fragment_shopping.*
+import javax.inject.Inject
 
-class AddShoppingItemFragment: Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+): Fragment(R.layout.fragment_add_shopping_item) {
 
     lateinit var shoppingViewModel: ShoppingViewModel
 
@@ -19,6 +27,14 @@ class AddShoppingItemFragment: Fragment(R.layout.fragment_add_shopping_item) {
         super.onViewCreated(view, savedInstanceState)
 
         shoppingViewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+        btnAddShoppingItem.setOnClickListener {
+            shoppingViewModel.insertShoppingItem(
+                etShoppingItemName.text.toString(),
+                etShoppingItemAmount.text.toString(),
+                etShoppingItemPrice.text.toString()
+            )
+        }
 
         ivShoppingImage.setOnClickListener {
            findNavController().navigate(
@@ -36,6 +52,39 @@ class AddShoppingItemFragment: Fragment(R.layout.fragment_add_shopping_item) {
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribleToObservers(){
+        shoppingViewModel.curImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(ivShoppingImage)
+        })
+
+        shoppingViewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when(result.status){
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            "Added Shopping Item",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+
+                        findNavController().popBackStack()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            result.message ?: "An unknown error occurred",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+
+                    Status.LOADING -> {
+                        /*NO OP*/
+                    }
+                }
+            }
+        })
     }
 
 }
